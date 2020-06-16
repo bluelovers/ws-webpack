66f0935c25a04d8e5278436c8d7bb3219accadaa
bluelovers <codelovers@users.sourceforge.net>
2020-06-16 20:28:01 +0800

    chore: update deps

--- packages/webpack-workspaces-support/nextjs.d.ts
+++ packages/webpack-workspaces-support/nextjs.d.ts
@@ -9,6 +9,7 @@ declare function withWorkspacesSupport<N extends INextConfig>(nextConfig?: N): N
     webpack(config: Configuration, ...argv: any[]): Configuration;
 };
 declare namespace withWorkspacesSupport {
-    var default: typeof withWorkspacesSupport;
+    var _a: typeof withWorkspacesSupport;
+    export { _a as default };
 }
 export = withWorkspacesSupport;
